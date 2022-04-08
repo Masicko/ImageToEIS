32668e5f936702ace820352a43367280e512ea5c
milos <vojta.milos@gmail.com>
2022-04-08 15:49:25 +0200

    type stable, type declared, direct_lin_idx (no string)

--- src/material_matrix_to_lin_sys.jl
+++ src/material_matrix_to_lin_sys.jl
@@ -1,16 +1,31 @@
-
-function get_I_name(x,y)
-    return "I_$(x)_$(y)"
+# idx in A_aux matrix
+# assuming that x, y can be in [1, 2, ... m*n+3]
+function aux_matrix_connectivity_entries_to_lin_idx(x,y, m,n)
+    if (x, y) == (1, 2)
+      return 1
+    elseif x == 2
+      # y - 2 + 1
+      return y - 1
+    elseif y == m*n + 3
+      #          y                - 2 + 1 ... for special case of right edge 
+      # (m*n + 3) + mod(x - 3, m) - 2 + 1 
+      return m*n + 2 + mod(x - 3, m)
+    # general case within the matrix
+    # ... horizontal ... indexed first
+    elseif mod(x - y, m) == 0
+      return y - 1
+    # ... vertical 
+    else
+      # max index for horiz. case  +  idx of x  -  col_number (because there are 1 less vertical connections than #of rows)
+      # (m*n + 3) + m - 2          +  x - 2     -  mod(x-3, n)
+      return m*(n + 1) + x - 1 - div(x-3, m)
+    end
 end
 
-function get_Z_name(x,y)
-    return "Z_$(x)_$(y)"
-end
-
-
+max_lin_idx(m,n) = aux_matrix_connectivity_entries_to_lin_idx(m*n + 1, m*n + 2, m, n)
 
 # auxilary_A has 2 on left, m*n+3 on the right, -1 on top and bottom
-function dict_to_lin_sys(Z_dict, auxilary_A)
+function vector_to_lin_sys(Z_vector, auxilary_A)
   matrix_header = String[]
   
   equation_rows = []
@@ -35,13 +50,9 @@ function dict_to_lin_sys(Z_dict, auxilary_A)
       end
   end
   
-  function add_value_to_matrix_row(matrix_header, equation_row, I_name, value)
-      idx = find_idx(I_name, matrix_header)
-      if idx == -1
-        idx = length(matrix_header) + 1
-        push!(matrix_header, I_name)
-      end
-      push!(equation_row, (idx, value))
+  function add_value_to_matrix_row(matrix_header, equation_row, previous_id, next_id, value)
+      lin_idx = aux_matrix_connectivity_entries_to_lin_idx(previous_id, next_id, m, n)     
+      push!(equation_row, (lin_idx, value))
   end
   
   # TODO optimize
@@ -81,20 +92,17 @@ function dict_to_lin_sys(Z_dict, auxilary_A)
   function add_equation_I_row(i, j)
     new_row = []
     act_id = auxilary_A[i+1, j+1]    
-    for node_id in previous(i,j)        
-        I_name = get_I_name(node_id, act_id)
-        add_value_to_matrix_row(matrix_header, new_row, I_name, -1)
+    for node_id in previous(i,j)                
+        add_value_to_matrix_row(matrix_header, new_row, node_id, act_id, -1)
     end
-    for node_id in next(i,j)
-        I_name = get_I_name(act_id, node_id)
-        add_value_to_matrix_row(matrix_header, new_row, I_name, 1)
+    for node_id in next(i,j)        
+        add_value_to_matrix_row(matrix_header, new_row, act_id, node_id, 1)
     end
 
     sort!(new_row, by = first)
     
     sys_row_idx += 1
-    add_row_to_sparse_input!(sparse_input, new_row, sys_row_idx)
-    
+    add_row_to_sparse_input!(sparse_input, new_row, sys_row_idx)    
     push!(RHS, 0.0) 
     return
   end
@@ -143,42 +151,32 @@ function dict_to_lin_sys(Z_dict, auxilary_A)
           add_value_to_matrix_row(
               matrix_header, 
               new_row, 
-              get_I_name(idx1, idx2),
-              Z_dict[get_Z_name(idx1, idx2)]
+              idx1, idx2,
+              Z_vector[aux_matrix_connectivity_entries_to_lin_idx(idx1, idx2, m, n)]
           )
       end
-    
+      
       sort!(new_row, by = first)
     
       sys_row_idx += 1
-      add_row_to_sparse_input!(sparse_input, new_row, sys_row_idx)
-      
+      add_row_to_sparse_input!(sparse_input, new_row, sys_row_idx)      
       push!(RHS, 1.0)
-  end
-  
-  return matrix_header, sparse_input, RHS
-  
-#   sys_A = Matrix(undef, length(equation_rows), length(matrix_header)-1)
-#   sys_A .= 0
-#   sys_b = Vector(undef, length(equation_rows))
-#   sys_b .= 0
-#   for i in 1:size(sys_A)[1]
-#       sys_A[i,1:length(equation_rows[i])-1] = equation_rows[i][2:end]
-#       sys_b[i] = equation_rows[i][1]
-#   end
-#   return matrix_header, sys_A, sys_b
+  end  
+  return matrix_header, sparse_input, RHS  
 end
 
 
 
 
 function get_auxilary_graph_matrix(m, n)
-    a = Vector(undef, m*n)
-    for i in 1:length(a)
-        a[i] = i+2
+    A = Matrix{Int64}(undef, m+2, n+2)
+    #
+    aux_vec = Vector{Int64}(undef, m*n)
+    for i in 1:length(aux_vec)
+        aux_vec[i] = i+2
     end
-    pre_A = reshape(a, m, n)
-    A = Matrix(undef, m+2, n+2)
+    pre_A = reshape(aux_vec, m, n)
+    #
     A[2:end-1, 2:end-1] = pre_A
     A[:, 1] .= 2
     A[:, end] .= m*n+3
@@ -190,7 +188,7 @@ end
 
 function get_large_material_matrix(pre_A)
     (m,n) = size(pre_A)
-    A = Matrix(undef, m+2, n+2)
+    A = Matrix{Int64}(undef, m+2, n+2)
     A[2:end-1, 2:end-1] = pre_A
     A[:, 1] .= i_LSM
     A[:, end] .= i_LSM
@@ -202,14 +200,14 @@ end
 
 
 function material_matrix_to_lin_sys(
-            material_A::Matrix=[0 0 0; 1 1 1], 
+            material_A::Matrix{<:Integer}=[0 0 0; 1 1 1],
             p::parameters=parameters()
     )
     (m,n) = size(material_A)
     aux_A = get_auxilary_graph_matrix(m,n)
     large_material_A = get_large_material_matrix(material_A)
     
-    function add_Z_to_dict(i,j, k, l)
+    function add_Z_to_vector(i,j, k, l)
         n1 = large_material_A[i,j]
         n2 = large_material_A[i+k, j+l]
         if n2 == -1
@@ -223,29 +221,32 @@ function material_matrix_to_lin_sys(
             idx1 = idx2
             idx2 = aux
         end
-        current_key = get_Z_name(idx1, idx2)
-        if haskey(Z_dict, current_key)            
-            f = deepcopy(Z_dict[current_key])            
-            Z_dict[current_key] = w -> f(w) +
-                get_Z_entry_from_material_matrix_codes(n1, n2, p)(w)
+        
+        current_key = aux_matrix_connectivity_entries_to_lin_idx(idx1, idx2, m, n)
+        if Z_vector[current_key] == 0
+            Z_vector[current_key] = 
+                get_Z_entry_from_material_matrix_codes(n1, n2, p)            
         else            
-            Z_dict[current_key] = 
-                get_Z_entry_from_material_matrix_codes(n1, n2, p)
-        end        
+            f = deepcopy(Z_vector[current_key])            
+            Z_vector[current_key] = w -> f(w) +
+                get_Z_entry_from_material_matrix_codes(n1, n2, p)(w)
+        end
     end
     
-    Z_dict = Dict()
+    Z_vector = Vector{Union{Function}}(undef, max_lin_idx(m,n))
+    Z_vector .= w -> 0
     for i in 2:m+1
         for j in 2:n+1
-            add_Z_to_dict(i,j, +1, 0) 
-            add_Z_to_dict(i,j, -1, 0)
-            add_Z_to_dict(i,j, 0, +1)
-            add_Z_to_dict(i,j, 0, -1)
+            add_Z_to_vector(i,j, +1, 0) 
+            add_Z_to_vector(i,j, -1, 0)
+            add_Z_to_vector(i,j, 0, +1)
+            add_Z_to_vector(i,j, 0, -1)
         end
     end
     for i in 2:m+1
-        add_Z_to_dict(i,1, 0, +1)
-        add_Z_to_dict(i,n+2, 0, -1)
+        add_Z_to_vector(i,1, 0, +1)
+        add_Z_to_vector(i,n+2, 0, -1)
     end    
-    return dict_to_lin_sys(Z_dict, aux_A)    
+    #return Z_vector
+    return vector_to_lin_sys(Z_vector, aux_A)    
 end
