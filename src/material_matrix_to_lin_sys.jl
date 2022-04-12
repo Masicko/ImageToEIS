a4c2d9a8d53cdce527d83c2a80f7b8d104a734c0
milos <vojta.milos@gmail.com>
2022-04-12 14:03:01 +0200

    modulat formualtion ready for 3D upgrade

--- src/material_matrix_to_lin_sys.jl
+++ src/material_matrix_to_lin_sys.jl
@@ -32,6 +32,23 @@ max_lin_idx(m,n) = aux_matrix_connectivity_entries_to_lin_idx(m*n + 1, m*n + 2, 
 
 
 
+function add_value_to_matrix_row(matrix_header, equation_row, previous_id, next_id, value, m, n)
+    lin_idx = aux_matrix_connectivity_entries_to_lin_idx(previous_id, next_id, m, n)     
+    push!(equation_row, (lin_idx, value))
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 get_previous_list() = [(0, -1), (-1, 0)]
 get_next_list() = [(1, 0), (0, 1)]
@@ -60,11 +77,6 @@ function next(i,j, auxilary_A)
     deleteat!(res, findall(x->x==-1,res))
     return res
   end
-end
-
-function add_value_to_matrix_row(matrix_header, equation_row, previous_id, next_id, value, m, n)
-    lin_idx = aux_matrix_connectivity_entries_to_lin_idx(previous_id, next_id, m, n)     
-    push!(equation_row, (lin_idx, value))
 end
 
 function add_row_to_sparse_input!(sparse_input,
@@ -96,6 +108,26 @@ function add_equation_I_row(i, j, auxilary_A, m, n, matrix_header, sys_row_idx, 
   push!(RHS, 0.0) 
   return
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function add_U_eq_for_each_relevant_path(Z_vector, auxilary_A, m, n, matrix_header, sys_row_idx, sparse_input, RHS)
@@ -179,6 +211,40 @@ end
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function get_auxilary_graph_matrix(m, n)
     A = Matrix{Int64}(undef, m+2, n+2)
     #
@@ -208,6 +274,31 @@ function get_large_material_matrix(pre_A)
     return A
 end
 
+function add_Z_to_vector!(Z_vector, i,j, k, l, aux_A, large_material_A, p, m, n)
+    n1 = large_material_A[i,j]
+    n2 = large_material_A[i+k, j+l]
+    if n2 == -1
+        return  
+    end
+    
+    idx1 = aux_A[i, j]
+    idx2 = aux_A[i+k, j+l]
+    if idx2 < idx1
+        aux = idx1
+        idx1 = idx2
+        idx2 = aux
+    end
+    
+    current_key = aux_matrix_connectivity_entries_to_lin_idx(idx1, idx2, m, n)
+    if Z_vector[current_key] == 0
+        Z_vector[current_key] = 
+            get_Z_entry_from_material_matrix_codes(n1, n2, p)            
+    else            
+        f = deepcopy(Z_vector[current_key])            
+        Z_vector[current_key] = w -> f(w) +
+            get_Z_entry_from_material_matrix_codes(n1, n2, p)(w)
+    end
+end
 
 
 function material_matrix_to_lin_sys(
@@ -218,45 +309,18 @@ function material_matrix_to_lin_sys(
     aux_A = get_auxilary_graph_matrix(m,n)
     large_material_A = get_large_material_matrix(material_A)
     
-    function add_Z_to_vector(i,j, k, l)
-        n1 = large_material_A[i,j]
-        n2 = large_material_A[i+k, j+l]
-        if n2 == -1
-            return
-        end
-        
-        idx1 = aux_A[i, j]
-        idx2 = aux_A[i+k, j+l]
-        if idx2 < idx1
-            aux = idx1
-            idx1 = idx2
-            idx2 = aux
-        end
-        
-        current_key = aux_matrix_connectivity_entries_to_lin_idx(idx1, idx2, m, n)
-        if Z_vector[current_key] == 0
-            Z_vector[current_key] = 
-                get_Z_entry_from_material_matrix_codes(n1, n2, p)            
-        else            
-            f = deepcopy(Z_vector[current_key])            
-            Z_vector[current_key] = w -> f(w) +
-                get_Z_entry_from_material_matrix_codes(n1, n2, p)(w)
-        end
-    end
-    
     Z_vector = Vector{Union{Function}}(undef, max_lin_idx(m,n))
     Z_vector .= w -> 0
     for i in 2:m+1
-        for j in 2:n+1
-            add_Z_to_vector(i,j, +1, 0) 
-            add_Z_to_vector(i,j, -1, 0)
-            add_Z_to_vector(i,j, 0, +1)
-            add_Z_to_vector(i,j, 0, -1)
+      for j in 2:n+1
+        for d in vcat(get_previous_list(), get_next_list())          
+          add_Z_to_vector!(Z_vector, i,j, d..., aux_A, large_material_A, p, m, n)
         end
+      end
     end
     for i in 2:m+1
-        add_Z_to_vector(i,1, 0, +1)
-        add_Z_to_vector(i,n+2, 0, -1)
+      add_Z_to_vector!(Z_vector, i,1, 0, +1, aux_A, large_material_A, p, m, n)
+      add_Z_to_vector!(Z_vector, i,n+2, 0, -1, aux_A, large_material_A, p, m, n)
     end    
     #return Z_vector
     return vector_to_lin_sys(Z_vector, aux_A)    
